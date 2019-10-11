a12748e01b3e327b19914b43f0767810bb422c30
vincgonzo@hotmail.fr <vincgonzo@hotmail.fr>
2019-10-11 15:48:08 +0200

    Article UD Action implements

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -8,9 +8,11 @@ use AppBundle\Representation\Articles;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends FOSRestController
 {
@@ -57,7 +59,7 @@ class ArticleController extends FOSRestController
 
     /**
      * @Rest\Get(
-     *     path = "/articles/{id}",
+     *     path = "/article/{id}",
      *     name = "app_article_show",
      *     requirements = {"id"="\d+"}
      * )
@@ -66,6 +68,58 @@ class ArticleController extends FOSRestController
     public function showAction(Article $article)
     {
         return $article;
+    }
+
+    /**
+     * @Rest\Post(
+     *     "/article/update/{post_id}",
+     *     name = "app_article_update",
+     *     requirements={"post_id" = "\d+"}
+     * )
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     * @Rest\View(statusCode=201)
+     */
+    public function updateAction($post_id, Article $article, ConstraintViolationList $violations)
+    {
+        if (count($violations)) {
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($violations as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+            
+            throw new ResourceValidationException($message);
+        }
+        
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Article')->find($post_id);
+        $repo->setTitle($article->getTitle());
+        $repo->setContent($article->getContent());
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($repo);
+        $em->flush();
+
+        return $repo;
+    }
+
+    /**
+     * @Rest\Post(
+     *     "/article/delete/{post_id}",
+     *     name = "app_article_delete",
+     *     requirements={"post_id" = "\d+"}
+     * )
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     * @Rest\View(statusCode=201)
+     */
+    // This particular route would not be accessible without auth process
+    public function deleteAction($post_id, Article $article)
+    {        
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Article')->find($post_id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($repo);
+        $em->flush();
+
+        return;
     }
 
     /**
--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -22,7 +22,6 @@ class Article
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      *
-     * @Expose
      */
     private $id;
 
